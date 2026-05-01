4b01e77146a25aee4ae0b7016f4921906392784e
beous <24703861.trang@student.iuh.edu.vn>
2026-05-01 21:48:47 +0700

    Update SuatChieu.java

--- entity/SuatChieu.java
+++ entity/SuatChieu.java
@@ -1,23 +1,28 @@
 package entity;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+
 public class SuatChieu {
     private String maSuat;
     private String maPhim;
     private String phongChieu;
-    private String ngayChieu; // Dùng String cho dễ xử lý giao diện
-    private String gioChieu;
+    private LocalDate ngayChieu; 
+    private LocalTime gioChieu;
 
-    public SuatChieu(String maSuat, String maPhim, String phongChieu, String ngayChieu, String gioChieu) {
+    public SuatChieu(String maSuat, String maPhim, String phongChieu, LocalDate ngay, LocalTime gio) {
         this.maSuat = maSuat;
         this.maPhim = maPhim;
         this.phongChieu = phongChieu;
-        this.ngayChieu = ngayChieu;
-        this.gioChieu = gioChieu;
+        this.ngayChieu = ngay;
+        this.gioChieu = gio;
     }
 
     public String getMaSuat() { return maSuat; }
     public String getMaPhim() { return maPhim; }
     public String getPhongChieu() { return phongChieu; }
-    public String getNgayChieu() { return ngayChieu; }
-    public String getGioChieu() { return gioChieu; }
+    public LocalDate getNgayChieu() { return ngayChieu; }
+    public LocalTime getGioChieu() { return gioChieu; }
 }
